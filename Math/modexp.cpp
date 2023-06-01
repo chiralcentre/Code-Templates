a44f8aa0e8ec26010b51f43313fb8828616c7298
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2023-06-02 00:47:23 +0800

    Update modexp.cpp with proper modulo operations

--- Math/modexp.cpp
+++ Math/modexp.cpp
@@ -4,8 +4,10 @@ ll modExp(ll x, ll y, ll p) {
     ll ans = 1;
     while (y > 0) {
         if (y % 2 == 1) ans *= x;
+        ans %= p;
         y >>= 1;
         x *= x;
+        x %= p;
     }
     return ans % p;
 }
