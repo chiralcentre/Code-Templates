b24f611bdda14b6beb8278919b4a9d1c100340a8
Ryan Tan <83827461+chiralcentre@users.noreply.github.com>
2023-07-08 22:26:40 +0800

    Update modexp.cpp to include code for modular multiplication

--- Math/modexp.cpp
+++ Math/modexp.cpp
@@ -11,3 +11,16 @@ ll modExp(ll x, ll y, ll p) {
     }
     return ans % p;
 }
+
+
+ll mulmod(ll a, ll b, ll mod) {
+    ll res = 0; // Initialize result
+    a %= mod;
+    while (b > 0) {
+        if (b % 2 == 1) res = (res + a) % mod;
+        a = (a << 1) % mod;
+        // Divide b by 2
+        b >>= 1;
+    }
+    return res % mod;
+}
